8368c7ff9621f6ad2c2d6f29406f550edf1091b0
vishhy <sharma.vishwas7788@gmail.com>
2023-01-01 15:25:50 +0530

    2

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ from typing import List
 
 
 REQUIREMENT_FILE_NAME="requirements.txt"
-HYPHEN_E_DOT="-e ."
+HYPHEN_E_DOT="-e ."   # Editable Installation
 
 def get_requirements()->List[str]:
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
 
+    # Replacing \n at the end of every package name
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
 
+    # Removing -e ., required for installing the code as a package, will trigger the setup.py file
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
